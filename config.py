cc5a36eebc8a026d7ea93936d73c60dd480bc142
Narain_karthik.jayakumar <78994599+narain-karthik@users.noreply.github.com>
2026-04-29 12:49:40 +0530

    Remove sensitive API and SMTP credentials
    Cleared sensitive configuration values for security.

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 # Configuration for AI Powered Firewall Monitoring
 
 # --- FortiGate 100F API Settings ---
-FGT_IP = "192.160.207.176:4449"
-FGT_API_KEY = "rhrfbfG5NQ9pQmzfb55yt98wgHfQxh"
+FGT_IP = ""
+FGT_API_KEY = ""
 
 # --- AI Settings (Local Ollama / Cloud API) ---
 OLLAMA_MODEL = "phi3:latest"
 OLLAMA_URL = "http://localhost:11434/api/generate"
-OLLAMA_API_KEY = "4c35061a53ea4586a6c1cdc5c680cc80.wv7x4W5BOD4kzO_OYvyPBRlW"
+OLLAMA_API_KEY = ""
 
 # --- SMTP / Email Settings ---
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 # The User's provided SMTP Account Details
-SMTP_USER = "narainjkans@gmail.com" 
-SMTP_PASS = "lugnxurevlqacssw" # App Password
-RECIPIENT_MAIL = "narainjkans1@gmail.com"
+SMTP_USER = "" 
+SMTP_PASS = "" # App Password
+RECIPIENT_MAIL = ""
 
 # --- System Settings ---
 POLL_INTERVAL_MINS = 30
